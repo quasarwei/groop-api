c1c8417b48342d147a9cf682d26232c94b81c09a
Quasar Wei <quasar.wei@outlook.com>
2019-11-26 09:49:45 -0800

    setup tables and start seed

--- seeds/seed.groop_users.sql
+++ seeds/seed.groop_users.sql
@@ -4,8 +4,24 @@ TRUNCATE
     groop_users
     RESTART IDENTITY CASCADE;
 
+TRUNCATE
+    groop_groups
+    RESTART IDENTITY CASCADE;
+
+TRUNCATE 
+    groop_tasks
+    RESTART IDENTITY CASCADE;
+
+
 INSERT INTO groop_users (username, fullname, password, email)
 VALUES
-  ('brock', 'Brock Boutwell', 'password', 'brockb@gmail.com');
+  ('username1', 'first1 last1', 'password', 'user1@email.com'),
+  ('username2', 'first2 last2', 'password', 'user2@email.com'),
+  ('username3', 'first3 last3', 'password', 'user3@email.com');
+  ('username4', 'first4 last4', 'password', 'user4@email.com');
+  ('username5', 'first5 last5', 'password', 'user5@email.com');
+  ('username6', 'first6 last6', 'password', 'user6@email.com');
+
+INSERT INTO groop_groups ()
 
 COMMIT;
