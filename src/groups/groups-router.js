47045f70c0362bb5101e32d6ab5c198e677bce03
stevenr1 <stevenr.thinkful@gmail.com>
2019-11-27 12:21:14 -0600

    updates to router

--- src/groups/groups-router.js
+++ src/groups/groups-router.js
@@ -10,4 +10,23 @@ const groupFormat = group => ({
 
 groupsRouter.post(':/group', jsonParser, async (req, res, next)=> {
     const { name, owner_id } = req.body;
+
+    for (const field of ['name', 'owner_id'])
+    if (!req.body[field])
+    return res.status(400).json({
+        error: `Missing '${field}' in request body`,
+    });
+
+    const newGroupInfo = {name, owner_id};
+    try {
+        const newGroup = await GroupService.postNewGroup(req.app.get('db'), newGroupInfo);
+        res.status(201)
+        .location(path.posix.join(req.originUrl, `/${newGroup.id}`))
+        .json(groupFormat(newGroup));
+    }
+    catch (error) {
+        next(error);
+    }
 })
+
+module.exports = groupsRouter;
