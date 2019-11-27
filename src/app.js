d41aaa761079c4bec46c934e6f7dd15f7b8ce75a
stevenr1 <stevenr.thinkful@gmail.com>
2019-11-27 13:31:59 -0600

    few tweaks to router

--- src/app.js
+++ src/app.js
@@ -7,8 +7,7 @@ const { NODE_ENV } = require('./config');
 
 const authRouter = require('./auth/auth-router');
 const usersRouter = require('./users/users-router');
-const groupsRouter = require('./groups/groups-router');
-
+const tasksRouter = require('./tasks/tasks-router');
 
 const app = express();
 
@@ -23,7 +22,7 @@ app.use(cors());
 
 app.use('/api/auth', authRouter);
 app.use('/api/users', usersRouter);
-app.use('/api/groups', groupsRouter);
+app.use('/api/tasks', tasksRouter);
 
 
 app.use(function errorHandler(error, req, res, next) { //eslint-disable-line no-unused-vars
